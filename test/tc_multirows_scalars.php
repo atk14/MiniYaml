5f61598ea9c7a4e1d94f2b874d95667bbfe4ac98
Jaromir Tomek <jaromir.tomek@gmail.com>
2026-05-17 14:03:10 +0200

    Testing

--- test/tc_multirows_scalars.php
+++ test/tc_multirows_scalars.php
@@ -1,7 +1,7 @@
 <?php
 class TcMultirowsScalars extends TcBase {
 
-	function test(){
+	function test_load(){
 		$src = '
 ---
 key: |
@@ -30,5 +30,48 @@ key: |
 		'),trim(miniYAML::Dump([
 			"key" => "line 1\nline 2",
 		])));
+	}
+
+	function test_in_the_middle_of_hash(){
+		// Literal block scalar between other keys
+		$src = '
+---
+key1: value1
+key2: |
+  line 1
+  line 2
+key3: value3
+		';
+		$ar = miniYAML::Load($src);
+		$this->assertEquals([
+			"key1" => "value1",
+			"key2" => "line 1\nline 2",
+			"key3" => "value3",
+		],$ar);
+
+		// Folded block scalar between other keys
+		$src = '
+---
+key1: value1
+key2: >
+  line 1
+  line 2
+key3: value3
+		';
+		$ar = miniYAML::Load($src);
+		$this->assertEquals([
+			"key1" => "value1",
+			"key2" => "line 1 line 2",
+			"key3" => "value3",
+		],$ar);
+	}
+
+	function test_roundtrip(){
+		$original = [
+			"key1" => "value1",
+			"key2" => "line 1\nline 2",
+			"key3" => "value3",
+		];
+		$this->assertEquals($original,miniYAML::Load(miniYAML::Dump($original)));
 	}
 }
